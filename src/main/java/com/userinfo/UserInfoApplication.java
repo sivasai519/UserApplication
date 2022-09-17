d4b2c6903280d32031a5bd2e7377471f61cb82cf
sivasai519 <sivasaikumar519@gmail.com>
2022-09-17 22:46:06 +0530

    removed comment in main class

--- src/main/java/com/userinfo/UserInfoApplication.java
+++ src/main/java/com/userinfo/UserInfoApplication.java
@@ -3,7 +3,6 @@ package com.userinfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**test commit */
 @SpringBootApplication
 public class UserInfoApplication {
 
